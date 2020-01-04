f04796be8250e738678876b46c4ba92d861c50bf
Louison <louison59279@gmail.com>
2020-01-04 16:20:03 +0100

    Home fonctionnel + responsive

---
+++ meatic/src/Components/Title.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "../Styles.css" 
+
+
+
+class Title extends Component {
+  render() {
+
+    return (
+        <div id="conteneur">
+            <div class="title">Meatic</div>
+            <div class="slogan">Rencontrez le plat de votre vie</div>
+        </div>
+    );
+}
+}
+
+export default Title;
